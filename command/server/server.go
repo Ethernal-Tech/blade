d65569533b4f766d36a25286f1270aaea6ddc2c2
oliverbundalo <oliver.bundalo@ethernal.tech>
2024-12-04 17:00:26 +0100

    Task 281: json-rpc flag rename to jsonrpc

--- command/server/server.go
+++ command/server/server.go
@@ -211,14 +211,14 @@ func setFlags(cmd *cobra.Command) {
 		&params.rawConfig.JSONRPCBatchRequestLimit,
 		jsonRPCBatchRequestLimitFlag,
 		defaultConfig.JSONRPCBatchRequestLimit,
-		"max length to be considered when handling json-rpc batch requests, value of 0 disables it",
+		"max length to be considered when handling jsonrpc batch requests, value of 0 disables it",
 	)
 
 	cmd.Flags().Uint64Var(
 		&params.rawConfig.JSONRPCBlockRangeLimit,
 		jsonRPCBlockRangeLimitFlag,
 		defaultConfig.JSONRPCBlockRangeLimit,
-		"max block range to be considered when executing json-rpc requests "+
+		"max block range to be considered when executing jsonrpc requests "+
 			"that consider fromBlock/toBlock values (e.g. eth_getLogs), value of 0 disables it",
 	)
 
