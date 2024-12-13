fb598f266d0f1fb99d005e0df77b7b87e10f55ac
filip <grujicf998@gmail.com>
2024-12-13 13:46:54 +0100

    Add bridge relayer cli command

---
+++ command/bridge-relayer/params.go
@@ -0,0 +1,15 @@
+package relayer
+
+// relayerParams defines all the configuration parameters necessary for the
+// bridge relayer to function properly.
+type relayerParams struct {
+	// internalChainRPC represents the RPC endpoint of the internal blockchain
+	// network (Blade).
+	internalChainRPC string
+
+	// relayerPrivateKey contains the cryptographic private key utilized by the
+	// relayer to sign and send transactions.
+	// SECURITY WARNING: This key must be kept strictly confidential and never
+	// exposed or commited to any public service or version control system.
+	relayerPrivateKey string
+}
---
+++ command/bridge-relayer/relayer.go
@@ -0,0 +1,48 @@
+package relayer
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var params relayerParams
+
+// GetCommand returns new bridge relayer [*cobra.Command].
+func GetCommand() *cobra.Command {
+	relayerCmd := &cobra.Command{
+		Use:   "bridge-relayer",
+		Short: "Bridge Relayer command starts new bridge relayer responsible for cross-chain asset transfers.",
+		Run:   runCommand,
+	}
+
+	setFlags(relayerCmd)
+
+	return relayerCmd
+}
+
+func runCommand(*cobra.Command, []string) {
+	fmt.Println("Bridge relayer is running...")
+}
+
+func setFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVarP(
+		&params.internalChainRPC,
+		"internal-chain-rpc",
+		"i",
+		"",
+		"internal chain rpc endpoint",
+	)
+
+	cmd.MarkFlagRequired("internal-chain-rpc")
+
+	cmd.Flags().StringVarP(
+		&params.relayerPrivateKey,
+		"private-key",
+		"k",
+		"",
+		"relayer's private key",
+	)
+
+	cmd.MarkFlagRequired("private-key")
+}
