919229f43be565e7609de2f5b5e66865936d1e34
luka-ciric-ethernal <luka.ciric@ethernal.tech>
2024-12-13 13:00:52 +0100

    External to internal erc20 erc721 erc1155

--- e2e-polybft/e2e/rollback_test.go
+++ e2e-polybft/e2e/rollback_test.go
@@ -38,9 +38,9 @@ func init() {
 	os.Setenv("E2E_LOG_LEVEL", "debug")
 }
 
-func TestE2E_TestRollback(t *testing.T) {
+func TestE2E_TestRollback_E2I(t *testing.T) {
 	const (
-		transfersCount        = 3
+		transfersCount        = 1
 		numBlockConfirmations = 2
 		// make epoch size long enough, so that all exit events are processed within the same epoch
 		epochSize             = 40
@@ -49,10 +49,11 @@ func TestE2E_TestRollback(t *testing.T) {
 		stateSyncedLogsCount  = 2 // map token and deposit
 		numberOfBridges       = 1
 		numberOfMapTokenEvent = 1
+		bridgeERC1155Amount   = 100
 	)
 
 	var (
-		bridgeAmount        = ethgo.Ether(2)
+		bridgeERC20Amount   = ethgo.Ether(2)
 		bridgeMessageResult contractsapi.BridgeMessageResultEvent
 	)
 
@@ -71,7 +72,7 @@ func TestE2E_TestRollback(t *testing.T) {
 		receiverKeys[i] = hex.EncodeToString(rawKey)
 		receiversAddrs[i] = key.Address()
 		receivers[i] = key.Address().String()
-		amounts[i] = fmt.Sprintf("%d", bridgeAmount)
+		amounts[i] = fmt.Sprintf("%d", bridgeERC20Amount)
 
 		t.Logf("Receiver#%d=%s\n", i+1, receivers[i])
 	}
@@ -112,34 +113,29 @@ func TestE2E_TestRollback(t *testing.T) {
 	bridgeCfg := polybftCfg.Bridge[chainID.Uint64()]
 	bridge := cluster.Bridges[0]
 
-	// txRelayer, err := txrelayer.NewTxRelayer(txrelayer.WithClient(childEthEndpoint))
-	// require.NoError(t, err)
+	txRelayer, err := txrelayer.NewTxRelayer(txrelayer.WithClient(childEthEndpoint))
+	require.NoError(t, err)
 
 	deployerKey, err := bridgeHelper.DecodePrivateKey("")
 	require.NoError(t, err)
 
-	deployTx := types.NewTx(types.NewLegacyTx(
-		types.WithTo(nil),
-		types.WithInput(contractsapi.RootERC20.Bytecode),
-	))
+	t.Run("Rollback_TestERC20", func(t *testing.T) {
+		deployTx := types.NewTx(types.NewLegacyTx(
+			types.WithTo(nil),
+			types.WithInput(contractsapi.RootERC20.Bytecode),
+		))
 
-	receipt, err := externalChainTxRelayer.SendTransaction(deployTx, deployerKey)
-	require.NoError(t, err)
-	require.NotNil(t, receipt)
-	require.Equal(t, uint64(types.ReceiptSuccess), receipt.Status)
+		receipt, err := externalChainTxRelayer.SendTransaction(deployTx, deployerKey)
+		require.NoError(t, err)
+		require.NotNil(t, receipt)
+		require.Equal(t, uint64(types.ReceiptSuccess), receipt.Status)
 
-	rootERC20Token := types.Address(receipt.ContractAddress)
-	t.Log("External chain token address:", rootERC20Token)
+		rootERC20Token := types.Address(receipt.ContractAddress)
+		t.Log("External chain token address:", rootERC20Token)
 
-	// wait for a couple of sprints
-	finalBlockNum := 1 * sprintSize
-	require.NoError(t, cluster.WaitForBlock(finalBlockNum, 2*time.Minute))
-
-	t.Run("Bridge_TestE2I", func(t *testing.T) {
-		erc20balances := make(map[string]*big.Int)
-		for _, receiver := range receivers {
-			erc20balances[receiver] = erc20BalanceOf(t, types.StringToAddress(receiver), rootERC20Token, externalChainTxRelayer)
-		}
+		// wait for a couple of sprints
+		finalBlockNum := 1 * sprintSize
+		require.NoError(t, cluster.WaitForBlock(finalBlockNum, 2*time.Minute))
 
 		// trying deposit for rollback
 		require.NoError(t,
@@ -156,24 +152,196 @@ func TestE2E_TestRollback(t *testing.T) {
 				false,
 			))
 
-		finalBlockNum := 10 * sprintSize
 		// wait for a couple of sprints
+		finalBlockNum = 10 * sprintSize
 		require.NoError(t, cluster.WaitForBlock(finalBlockNum, 2*time.Minute))
 
 		// the bridge transactions are processed and there should be a success state sync events
 		logs, err := getFilteredLogs(bridgeMessageResult.Sig(), 0, finalBlockNum, childEthEndpoint)
 		require.NoError(t, err)
 
-		// assert that all deposits are executed successfully
-		// because of the token mapping with the first deposit
+		// assert that all deposits are rollbacked (no success events)
 		assertBridgeEventResultSuccess(t, logs, 0)
 
+		childERC20Token := getChildToken(t, contractsapi.RootERC20Predicate.Abi,
+			bridgeCfg.ExternalERC20PredicateAddr, rootERC20Token, externalChainTxRelayer)
+
 		for _, receiver := range receivers {
-			// assert that receiver's balance on RootERC20 smart contract is as expected
-			balance := erc20BalanceOf(t, types.StringToAddress(receiver), rootERC20Token, externalChainTxRelayer)
-			require.True(t, erc20balances[receiver].Cmp(balance) == 0)
+			balanceOfFn := &contractsapi.BalanceOfRootERC20Fn{Account: types.StringToAddress(receiver)}
+			balanceOfInput, err := balanceOfFn.EncodeAbi()
+			require.NoError(t, err)
+
+			balanceRaw, err := txRelayer.Call(types.ZeroAddress, childERC20Token, balanceOfInput)
+			require.NoError(t, err)
+			// Child token balance should be equal to 0x because rollback is executed
+			require.Equal(t, balanceRaw, "0x")
+		}
+		t.Log("Deposits were successfully rollbacked")
+	})
+
+	t.Run("Rollback_TestERC721", func(t *testing.T) {
+		tokenIDs := make([]string, transfersCount)
+
+		for i := 0; i < transfersCount; i++ {
+			key, err := crypto.GenerateECDSAKey()
+			require.NoError(t, err)
+
+			rawKey, err := key.MarshallPrivateKey()
+			require.NoError(t, err)
+
+			receiverKeys[i] = hex.EncodeToString(rawKey)
+			receivers[i] = key.Address().String()
+			receiversAddrs[i] = key.Address()
+			tokenIDs[i] = fmt.Sprintf("%d", i)
+
+			t.Logf("Receiver#%d=%s\n", i+1, receivers[i])
 		}
 
-		t.Log("Deposits were successfully rollbacked")
+		deployTx := types.NewTx(&types.LegacyTx{
+			BaseTx: &types.BaseTx{
+				To:    nil,
+				Input: contractsapi.RootERC721.Bytecode,
+			},
+		})
+
+		// deploy root ERC 721 token
+		receipt, err := externalChainTxRelayer.SendTransaction(deployTx, deployerKey)
+		require.NoError(t, err)
+
+		rootERC721Addr := types.Address(receipt.ContractAddress)
+
+		// DEPOSIT ERC721 TOKENS
+		// send a few transactions to the bridge
+		require.NoError(
+			t,
+			bridge.Deposit(
+				common.ERC721,
+				rootERC721Addr,
+				bridgeCfg.ExternalERC721PredicateAddr,
+				bridgeHelper.TestAccountPrivKey,
+				strings.Join(receivers, ","),
+				"",
+				strings.Join(tokenIDs, ","),
+				bridge.JSONRPCAddr(),
+				bridgeHelper.TestAccountPrivKey,
+				false),
+		)
+
+		// wait for a few more sprints
+		require.NoError(t, cluster.WaitForBlock(50, 4*time.Minute))
+
+		validatorSrv := cluster.Servers[0]
+		childEthEndpoint := validatorSrv.JSONRPC()
+
+		// the transactions are processed and there should be a success events
+		var bridgeMessageResult contractsapi.BridgeMessageResultEvent
+
+		logs, err := getFilteredLogs(bridgeMessageResult.Sig(), 0, uint64(50+2*epochSize), childEthEndpoint)
+		require.NoError(t, err)
+
+		// It shouldn't transfer any token, it should rollback
+		assertBridgeEventResultSuccess(t, logs, 0)
+
+		// retrieve child token address (from both chains, and assert they are the same)
+		externalChildTokenAddr := getChildToken(t, contractsapi.RootERC721Predicate.Abi, bridgeCfg.ExternalERC721PredicateAddr,
+			rootERC721Addr, externalChainTxRelayer)
+
+		t.Log("External child token", externalChildTokenAddr)
+
+		owner := erc721OwnerOf(t, big.NewInt(0), externalChildTokenAddr, externalChainTxRelayer)
+		require.NotEqual(t, deployerKey.Address(), owner)
+
+		t.Log("Deposits were successfully rollback")
+	})
+
+	t.Run("Rollback_TestERC1155", func(t *testing.T) {
+		tokenIDs := make([]string, transfersCount)
+
+		for i := 0; i < transfersCount; i++ {
+			key, err := crypto.GenerateECDSAKey()
+			require.NoError(t, err)
+
+			rawKey, err := key.MarshallPrivateKey()
+			require.NoError(t, err)
+
+			receiverKeys[i] = hex.EncodeToString(rawKey)
+			receivers[i] = key.Address().String()
+			receiversAddrs[i] = key.Address()
+			amounts[i] = fmt.Sprintf("%d", bridgeERC1155Amount)
+			tokenIDs[i] = fmt.Sprintf("%d", i+1)
+
+			t.Logf("Receiver#%d=%s\n", i+1, receivers[i])
+		}
+
+		deployTx := types.NewTx(&types.LegacyTx{
+			BaseTx: &types.BaseTx{
+				To:    nil,
+				Input: contractsapi.RootERC1155.Bytecode,
+			},
+		})
+
+		receipt, err := externalChainTxRelayer.SendTransaction(deployTx, deployerKey)
+		require.NoError(t, err)
+
+		rootERC1155Addr := types.Address(receipt.ContractAddress)
+		require.NoError(
+			t,
+			bridge.Deposit(
+				common.ERC1155,
+				rootERC1155Addr,
+				bridgeCfg.ExternalERC1155PredicateAddr,
+				bridgeHelper.TestAccountPrivKey,
+				strings.Join(receivers, ","),
+				strings.Join(amounts, ","),
+				strings.Join(tokenIDs, ","),
+				bridge.JSONRPCAddr(),
+				bridgeHelper.TestAccountPrivKey,
+				false),
+		)
+
+		// wait for a few more sprints
+		require.NoError(t, cluster.WaitForBlock(50, 4*time.Minute))
+
+		validatorSrv := cluster.Servers[0]
+		childEthEndpoint := validatorSrv.JSONRPC()
+
+		// the transactions are processed and there should be a success events
+		var bridgeMessageResult contractsapi.BridgeMessageResultEvent
+
+		logs, err := getFilteredLogs(bridgeMessageResult.Sig(), 0, uint64(50+2*epochSize), childEthEndpoint)
+		require.NoError(t, err)
+
+		assertBridgeEventResultSuccess(t, logs, 0)
+
+		txRelayer, err := txrelayer.NewTxRelayer(txrelayer.WithClient(childEthEndpoint))
+		require.NoError(t, err)
+
+		// retrieve child token address
+		l1ChildTokenAddr := getChildToken(t, contractsapi.RootERC1155Predicate.Abi, bridgeCfg.ExternalERC1155PredicateAddr,
+			rootERC1155Addr, externalChainTxRelayer)
+		l2ChildTokenAddr := getChildToken(t, contractsapi.ChildERC1155Predicate.Abi, bridgeCfg.InternalERC1155PredicateAddr,
+			rootERC1155Addr, txRelayer)
+
+		t.Log("L1 child token", l1ChildTokenAddr)
+		t.Log("L2 child token", l2ChildTokenAddr)
+		// require.Equal(t, l1ChildTokenAddr, l2ChildTokenAddr)
+
+		// check receivers balances got increased by deposited amount
+		for i := range receivers {
+			balanceOfFn := &contractsapi.BalanceOfChildERC1155Fn{
+				Account: deployerKey.Address(),
+				ID:      big.NewInt(int64(i + 1)),
+			}
+
+			balanceInput, err := balanceOfFn.EncodeAbi()
+			require.NoError(t, err)
+
+			balanceRaw, err := externalChainTxRelayer.Call(types.ZeroAddress, l1ChildTokenAddr, balanceInput)
+			require.NoError(t, err)
+
+			require.Equal(t, balanceRaw, "0x")
+		}
+
+		t.Log("Deposits were successfully processed")
 	})
 }
