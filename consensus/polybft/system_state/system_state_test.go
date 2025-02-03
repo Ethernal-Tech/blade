cc74f056c35cde36b129dc437b367ca48bdd5e74
Dusan Nosovic <dusan.nosovic@ethernal.tech>
2025-02-03 12:57:22 +0100

    test fix

--- consensus/polybft/system_state/system_state_test.go
+++ consensus/polybft/system_state/system_state_test.go
@@ -122,16 +122,6 @@ func TestSystemState_GetBridgeBatchByNumber(t *testing.T) {
 			function setBridgeMessage(uint256 _num) public payable {
 				SignedBridgeMessageBatch storage signedBatch = batches[_num];
 
-			// Initialize an empty BridgeMessageBatch
-				signedBatch.batch = BridgeMessageBatch({
-					messages: new BridgeMessage,
-					sourceChainId: 1,
-					destinationChainId: 2,
-					threshold: 1,
-					isRollback: false
-				});
-
-
 				signedBatch.signature = [uint256(300), uint256(200)];
 				signedBatch.bitmap = "smth";
 				signedBatch.validatorSetBatchId = 1;
@@ -170,12 +160,9 @@ func TestSystemState_GetBridgeBatchByNumber(t *testing.T) {
 	sbmb, err := systemState.GetBridgeBatchByNumber(batchID)
 	require.NoError(t, err)
 
+	sbmb.Batch = nil
+
 	require.EqualValues(t, &contractsapi.SignedBridgeMessageBatch{
-		Batch: &contractsapi.BridgeMessageBatch{
-			SourceChainID:      big.NewInt(1),
-			DestinationChainID: big.NewInt(2),
-			Threshold:          big.NewInt(1),
-			IsRollback:         false},
 		Signature:           [2]*big.Int{big.NewInt(300), big.NewInt(200)},
 		Bitmap:              []byte("smth"),
 		ValidatorSetBatchID: big.NewInt(1),
