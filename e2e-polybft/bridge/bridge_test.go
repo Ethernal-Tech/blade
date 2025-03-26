c7c4cd6aba0f46f4696dce449bd0f6067a3f57ef
luka-ciric-ethernal <luka.ciric@ethernal.tech>
2025-03-26 09:06:42 +0100

    remove init

--- e2e-polybft/bridge/bridge_test.go
+++ e2e-polybft/bridge/bridge_test.go
@@ -4,9 +4,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"math/big"
-	"os"
 	"path"
-	"path/filepath"
 	"strings"
 	"testing"
 	"time"
@@ -2650,21 +2648,6 @@ func TestE2E_Bridge_InsufficientFunds(t *testing.T) {
 			}
 		}
 	})
-}
-
-func init() {
-	wd, err := os.Getwd()
-	if err != nil {
-		return
-	}
-
-	parent := filepath.Dir(wd)
-	parent = strings.Trim(parent, "e2e-polybft")
-	wd = filepath.Join(parent, "/artifacts/blade")
-	os.Setenv("EDGE_BINARY", wd)
-	os.Setenv("E2E_TESTS", "true")
-	os.Setenv("E2E_LOGS", "true")
-	os.Setenv("E2E_LOG_LEVEL", "debug")
 }
 
 func TestE2E_Bridge_WithdrawInsufficientFunds(t *testing.T) {
